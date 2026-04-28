812687032ac91b6a8a8bb975226959e2823548f8
AkaiNoIto <morgan.verspeek01@gmail.com>
2026-04-28 22:28:59 +0200

    fix: defer reply to avoid interaction timeout

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -11,13 +11,12 @@ module.exports = {
             const command = client.commands.get(interaction.commandName);
             if (!command) return;
             try {
+                await interaction.deferReply();
                 await command.execute(interaction, client);
             } catch (error) {
                 console.error(error);
-                if (interaction.replied || interaction.deferred) {
-                    await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
-                } else {
-                    await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+                if (interaction.deferred) {
+                    await interaction.editReply({ content: "There was an error while executing this command!" }).catch(() => {});
                 }
             }
         } else if (interaction.isButton()) {
